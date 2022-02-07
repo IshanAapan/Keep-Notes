62fd2c127a6cb545f1fc1b5bb89f8e51ed878f7e
Ishan <ishanaapan02@gmail.com>
2022-02-07 12:01:48 -0800

    2 files

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Notes-App");
+
+function add(){
+    let a=[];
+    let a=document.getElementById("sel").innerText;
+    a.forEach(function(element){
+        a.push(element);
+    })
+}
+// let a=[1];
+// a.forEach(function(element){
+//     console.log(element);
+//     // document.write(element);
+//     // alert(element);
+// });
