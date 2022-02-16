fbf701f05d12b35a15589bf2668152126480b8cc
IshanAapan <80051311+IshanAapan@users.noreply.github.com>
2022-02-16 03:16:00 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 console.log("Notes-App");
 
 function add(){
-    let a=[];
-    let a=document.getElementById("sel").innerText;
-    a.forEach(function(element){
-        a.push(element);
-    })
+//     let a=[];
+    var a=document.getElementById("sel").value;
+    console.log(a);
+//     a.forEach(function(element){
+//         a.push(element);
+//     })
 }
 // let a=[1];
 // a.forEach(function(element){
